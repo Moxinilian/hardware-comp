1837212276c65286d686b49200c27cb8e377b9f3
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2023-02-16 17:58:53 +0100

    clarify the scope of pdl.py

--- src/pdl.py
+++ src/pdl.py
@@ -1,3 +1,5 @@
+# This file is a placeholder while waiting for proper implementation of PDL in xDSL.
+
 from __future__ import annotations
 from xdsl.dialects.builtin import *
 from xdsl.ir import *
@@ -79,48 +81,5 @@ class TypeType(ParametrizedAttribute, MLIRType):
 class ValueType(ParametrizedAttribute, MLIRType):
     name = "pdl.value"
 
-
-@irdl_op_definition
-class AttributeOp(Operation):
-    """
-    https://mlir.llvm.org/docs/Dialects/PDLOps/#pdlattribute-mlirpdlattributeop
-    """
-    name: str = "pdl.attribute"
-    value: OptOpAttr[Attribute]
-    value_type: Annotated[OptOperand, TypeType]
-    output: Annotated[OpResult, AttributeType]
-
-
-@irdl_op_definition
-class EraseOp(Operation):
-    """
-    https://mlir.llvm.org/docs/Dialects/PDLOps/#pdlerase-mlirpdleraseop
-    """
-    name: str = "pdl.erase"
-    op_value: Annotated[Operand, OperationType]
-
-
-@irdl_op_definition
-class OperandOp(Operation):
-    """
-    https://mlir.llvm.org/docs/Dialects/PDLOps/#pdloperand-mlirpdloperandop
-    """
-    name: str = "pdl.operand"
-    value_type: Annotated[Operand, TypeType]
-    output: Annotated[OpResult, ValueType]
-
-
-@irdl_op_definition
-class OperandsOp(Operation):
-    """
-    https://mlir.llvm.org/docs/Dialects/PDLOps/#pdloperands-mlirpdloperandsop
-    """
-    name: str = "pdl.operands"
-    value_type: Annotated[Operand,
-                          RangeType]  # Range of Types can we parametrize this?
-    output: Annotated[OpResult,
-                      RangeType]  # Range of Values can we parametrize this?
-
-
-Pdl = Dialect([AttributeOp, OperandOp, EraseOp, OperandsOp],
+Pdl = Dialect([],
               [AttributeType, OperationType, RangeType, TypeType, ValueType])
