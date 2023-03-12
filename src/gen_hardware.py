c67e0f7a8cba98c770bedfc8767f5373ee4276d4
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2023-03-12 09:50:13 +0100

    refactored and added MLIR dependency

--- src/gen_hardware.py
+++ src/gen_hardware.py
@@ -3,11 +3,14 @@ from xdsl.dialects.arith import Arith
 from xdsl.dialects.builtin import Builtin, IntAttr
 from xdsl.printer import Printer
 from xdsl.parser import Parser, Source
+from subprocess import Popen, PIPE
 
-from pdl_interp import PdlInterp
-from pdl import Pdl, RangeType, RangeValue
-from fsm import Fsm
+from dialects.pdl_interp import PdlInterp
+from dialects.pdl import Pdl
+from dialects.fsm import Fsm
 
+MLIR_PDLL = "./mlir-pdll"
+MLIR_OPT = "./mlir-opt"
 
 context = MLContext()
 
@@ -15,12 +18,15 @@ context.register_dialect(Arith)
 context.register_dialect(Builtin)
 context.register_dialect(Pdl)
 context.register_dialect(PdlInterp)
+context.register_dialect(Fsm)
 
-with open('test_pdl.mlir', 'r') as file:
-    data = file.read()
+mlir_opt_process = Popen([MLIR_OPT, "-mlir-print-op-generic", "--convert-pdl-to-pdl-interp"], stdin=PIPE, stdout=PIPE)
+mlir_pdll_process = Popen([MLIR_PDLL, "rewrites/redundant_or.pdll", "-x=mlir"], stdout=mlir_opt_process.stdin)
+mlir_opt_process.stdin.close() # de-duplicate stdin handle
+pdl_interp_src = mlir_opt_process.stdout.read().decode()
 
-parser = Parser(context, data, source=Source.MLIR, filename="test_pdl.mlir")
-module = parser.parse_module()
+pdl_interp_parser = Parser(context, pdl_interp_src, source=Source.MLIR)
+pdl_interp_data = pdl_interp_parser.parse_module()
 
 printer = Printer(target=Printer.Target.MLIR)
-printer.print(module)
+printer.print(pdl_interp_data)
